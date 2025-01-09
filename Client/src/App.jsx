50d77443ac0d4d5d1d437dbd6efbc56e367f4ad4
JatinKumar65042 <jatinsokhal2300@gmail.com>
2025-01-10 01:04:40 +0530

    Add React Router

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='text-3xl flex justify-center items-center bg-black'>
+    <div className='text-3xl flex justify-center items-center'>
       <h1 className='m-6 bg-red-700'>Jatin</h1>
     </div>
   )
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout'
+import About from './Components/AboutPage/About'
+import { RouterProvider } from 'react-router'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path = "/" element = {<Layout/>}>
+      {/* <Route path = "" element = {} /> */}
+      <Route path = "about" element = {<About/>} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
+    {/* <App/> */}
   </StrictMode>,
 )
