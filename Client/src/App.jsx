8997af43a92e7e4e1d665c518a05ba1df0aae51f
JatinKumar65042 <jatinsokhal2300@gmail.com>
2025-01-10 00:41:06 +0530

    Header Footer File

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='text-3xl flex justify-center items-center bg-black'>
-      <h1 className='m-6 bg-red-700'>Jatin</h1>
-    </div>
+    <>
+
+    </>
   )
 }
 
---
+++ Client/src/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Components/Header/Header'
+import { Outlet } from 'react-router-dom'
+import Footer from './Components/Footer/footer'
+
+
+const Layout = () => {
+  return (
+    <div>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Layout
