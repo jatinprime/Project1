dd5a748e665d32c5f743871463a83c213049e0fd
jatinprime <jatinagrawal28112003@gmail.com>
2025-01-10 00:26:37 +0530

    first commit by agra

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className='text-3xl flex justify-center items-center bg-black'>
       <h1 className='m-6 bg-red-700'>Jatin</h1>
+      <h1 className='m-20'>Made by jatin agrawal</h1>
     </div>
   )
 }
