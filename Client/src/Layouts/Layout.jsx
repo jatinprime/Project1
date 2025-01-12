68f549172f848c953f18c15999eb6a2519b8ec1f
jatinprime <jatinagrawal28112003@gmail.com>
2025-01-12 19:13:48 +0530

    added use locator hook in layout.jsx

--- Client/src/Layouts/Layout.jsx
+++ Client/src/Layouts/Layout.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from '../Components/Header/Header'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Footer from '../Components/Footer/footer'
 
 
 const Layout = () => {
+  const location = useLocation() ;
+  const hideHeader = (location.pathname === '/register' || location.pathname === '/login') ;
   return (
     <div>
-      <Header/>
+      {!hideHeader && <Header/>}
       <Outlet/>
       <Footer/>
     </div>
