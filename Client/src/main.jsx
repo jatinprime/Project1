760066ff8eb1517b9001608d21b450e53d0af53e
JatinKumar65042 <jatinsokhal2300@gmail.com>
2025-01-12 15:58:02 +0530

    some more changes in main

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -32,5 +32,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}/>
     <h1>Changes in the main.jsx</h1>
+    <h1>changes 2</h1>
   </StrictMode>,
 )
