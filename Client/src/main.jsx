c5a3cede49cc609f97fb23883af2d9b25891e795
JatinKumar65042 <jatinsokhal2300@gmail.com>
2025-01-12 19:00:58 +0530

    finally done merge

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -7,9 +7,6 @@ import Layout from './Layouts/Layout'
 import { RouterProvider } from 'react-router'
 import About from './Pages/About'
 import Login from './Pages/Login.jsx'
-import Home from './Pages/Home.jsx'
-import Notfound from './Pages/Notfound.jsx'
-import PrivacyPolicy from './Pages/PrivacyPolicy.jsx'
 import Layout2 from './Layouts/Layout2'
 import Signup from './Pages/Signup.jsx'
 import Home from './Pages/Home.jsx'
@@ -20,11 +17,7 @@ import Contact from './Pages/contact.jsx'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path = "" element = {<Layout/>}>
-        <Route path = "/" element = {<Home/>}/>
-        <Route path = "/login" element = {<Login/>} />
-        <Route path = "/about" element = {<About/>} />
-        <Route path = "/privacy-policy" element = {<PrivacyPolicy/>} />
-        {/* <Route path = "/about" element = {<About/>} /> */}
+      <Route path = "/login" element = {<Login/>} />
       <Route path = "/" element = {<Home/>}/>
       <Route path = "/about" element = {<About/>} />
       <Route path = "/register" element = {<Signup/>} />
