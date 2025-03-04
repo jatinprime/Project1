106fb4f83a9b8429168e35dab0e8e9b4553d6221
jatinprime <jatinagrawal28112003@gmail.com>
2025-03-05 01:15:08 +0530

    add error handling and middleware for improved server stability

--- Server/index.js
+++ Server/index.js
@@ -1,20 +1,43 @@
 const express = require('express') ;
-const dotenv = require('dotenv');
+const dotenv = require('dotenv') ;
 const colors = require('colors') ;
+const morgan = require('morgan') ;
+const cors = require('cors') ;
 
 const app = express() ;
 
 dotenv.config() ;
 
-const PORT = 8000 ;
+app.use(cors()) ;
+app.use(morgan("dev")) ;
+app.use(express.json()) ;
 
+//Initial basic route
 app.get('/' , (req , res) => {
-    res.status(200).send({
-        success : true,
-        message : "initial",
-    })
+    try {
+        res.status(200).send({
+            success : true,
+            message : "initial",
+        })
+    } catch (error) {
+        console.log(error) ;
+        res.status(500).send({
+            success : false,
+            message : "Error in the home initial route",
+            error
+        })
+    }
 })
 
+const PORT = process.env.PORT || 8000 ;
+
 app.listen(PORT , () => {
-    console.log(`App is Listening Successfully on ${PORT}`.white.bgMagenta) ;
+    try {
+        app.listen(PORT, () => {
+            console.log(`App is Listening Successfully on ${PORT}`.white.bgMagenta);
+        });
+    } catch (error) {
+        console.error("Error starting the server:", error.message);
+        process.exit(1); // Exit the process if the server fails to start
+    }
 })
